6697552b296242c17c2e4bede167853cd040e5b0
Khyrstina Smith <khyrstinasmith@gmail.com>
2023-03-24 15:12:21 -0400

    Added PreviousIdentifications view as well as the associated controller that retrieves the image url from the database.

---
+++ BotaniFinder/Controllers/PreviousIdentificationsFromDatabaseController.cs
@@ -0,0 +1,26 @@
+﻿using BotaniFinder.Context;
+using BotaniFinder.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BotaniFinder.Controllers
+{
+
+    public class PreviousIdentificationsFromDatabaseController : Controller
+    {
+        private readonly BotaniFinderDbContext _context;
+
+        public PreviousIdentificationsFromDatabaseController(BotaniFinderDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var images = _context.Urls.Select(u => new Url
+            {
+                UrlId = u.UrlId,
+                O = u.O ?? ""
+            }).ToList();
+            return View(images);
+        }
+    }
+}
